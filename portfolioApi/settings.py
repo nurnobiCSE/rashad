3541057844bdb7cfbf6e8b653b4a2093312a8edc
nurnobiCSE <shuvo517942@gmail.com>
2022-03-02 11:27:32 +0600

    adding cors headers v-2

--- portfolioApi/settings.py
+++ portfolioApi/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 #importing for heroku app
 import django_heroku
+import corsheaders
 import dj_database_url
 from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
